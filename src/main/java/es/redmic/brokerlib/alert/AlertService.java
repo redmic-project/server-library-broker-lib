c837bd118ca790a59ab9f3562d2e11b469640ec6
Noel Alonso <noel@redmic.es>
2018-10-02 13:12:35 +0100

    Añade clave al envío de alertas

--- src/main/java/es/redmic/brokerlib/alert/AlertService.java
+++ src/main/java/es/redmic/brokerlib/alert/AlertService.java
@@ -2,6 +2,7 @@ package es.redmic.brokerlib.alert;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -26,9 +27,6 @@ public class AlertService {
 
 	public void errorAlert(String subject, String message) {
 
-		if (PROFILE_ACTIVE.equals("test"))
-			ALERT_EMAIL = "test@redmic.es";
-
 		String subjectDefault = "[ERROR][" + PROFILE_ACTIVE + "] ";
 		send(new Message(ALERT_EMAIL, subjectDefault + subject, message, AlertType.ERROR.name()));
 	}
@@ -37,6 +35,6 @@ public class AlertService {
 
 		logger.info("sending alert='{}' to topic='{}'", message, ALERT_TOPIC);
 
-		kafkaTemplate.send(ALERT_TOPIC, message);
+		kafkaTemplate.send(ALERT_TOPIC, String.valueOf(DateTime.now().getMillis()), message);
 	}
 }
