824d209c4ee7812715e8086559691205b3048a0b
Noel Alonso <noel@redmic.es>
2019-04-26 13:57:05 +0100

    Añade dto base para los datos de tipo dominio

---
+++ src/main/java/es/redmic/brokerlib/avro/data/common/DomainDTO.java
@@ -0,0 +1,53 @@
+package es.redmic.brokerlib.avro.data.common;
+
+/*-
+ * #%L
+ * broker-lib
+ * %%
+ * Copyright (C) 2019 REDMIC Project / Server
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import es.redmic.brokerlib.avro.common.CommonDTO;
+
+public abstract class DomainDTO extends CommonDTO {
+
+	@NotNull
+	@Size(min = 1, max = 150)
+	protected String name;
+
+	@NotNull
+	@Size(min = 1, max = 150)
+	private String name_en;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName_en() {
+		return this.name_en;
+	}
+
+	public void setName_en(String name_en) {
+		this.name_en = name_en;
+	}
+}
