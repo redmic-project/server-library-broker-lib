e942eef3a88fb1015b2a975295de0593a06933f4
Noel Alonso <noel@redmic.es>
2019-04-30 08:18:09 +0100

    Añade métodos hashCode y equals para domain

--- src/main/java/es/redmic/brokerlib/avro/data/common/DomainDTO.java
+++ src/main/java/es/redmic/brokerlib/avro/data/common/DomainDTO.java
@@ -27,6 +27,10 @@ import es.redmic.brokerlib.avro.common.CommonDTO;
 
 public abstract class DomainDTO extends CommonDTO {
 
+	public DomainDTO() {
+		super();
+	}
+
 	@NotNull
 	@Size(min = 1, max = 150)
 	protected String name;
@@ -49,5 +53,36 @@ public abstract class DomainDTO extends CommonDTO {
 
 	public void setName_en(String name_en) {
 		this.name_en = name_en;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((name_en == null) ? 0 : name_en.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DomainDTO other = (DomainDTO) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (name_en == null) {
+			if (other.name_en != null)
+				return false;
+		} else if (!name_en.equals(other.name_en))
+			return false;
+		return true;
 	}
 }
